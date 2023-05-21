6576a0750646f02212d188652f2f7063fb3d25f2
Altereyo <r8.lebz@gmail.com>
2023-05-21 18:17:01 +0300

    code: connection error screen

--- lib/screens/connection_error.dart
+++ lib/screens/connection_error.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_config/styles/app_colors.dart';
 
 class ConnectionErrorScreen extends StatelessWidget {
   const ConnectionErrorScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: FractionallySizedBox(
+          widthFactor: 0.8,
+          child: Text(
+            'A network connection is required to continue',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: AppColors.textColor,
+              fontSize: 24,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
