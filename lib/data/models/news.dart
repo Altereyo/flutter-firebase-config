374857cced17262039564f0d4930f81274be1f08
Altereyo <r8.lebz@gmail.com>
2023-05-21 17:01:48 +0300

    code: models

---
+++ lib/data/models/news.dart
@@ -0,0 +1,52 @@
+class News {
+  final String? title;
+  final String? description;
+
+//<editor-fold desc="Data Methods">
+  const News({
+    this.title,
+    this.description,
+  });
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      (other is News &&
+          runtimeType == other.runtimeType &&
+          title == other.title &&
+          description == other.description);
+
+  @override
+  int get hashCode => title.hashCode ^ description.hashCode;
+
+  @override
+  String toString() {
+    return 'News{ title: $title, description: $description,}';
+  }
+
+  News copyWith({
+    String? title,
+    String? description,
+  }) {
+    return News(
+      title: title ?? this.title,
+      description: description ?? this.description,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'title': this.title,
+      'description': this.description,
+    };
+  }
+
+  factory News.fromMap(Map<String, dynamic> map) {
+    return News(
+      title: map['title'] as String,
+      description: map['description'] as String,
+    );
+  }
+
+//</editor-fold>
+}
