5f6f96fc9f0700c12702a6a67bde0fb007900940
Altereyo <r8.lebz@gmail.com>
2023-05-21 18:16:45 +0300

    code: firebase remote config fetch settings

--- lib/core/functions/get_remote_config.dart
+++ lib/core/functions/get_remote_config.dart
@@ -4,8 +4,9 @@ Future<FirebaseRemoteConfig> getFirebaseRemoteConfig() async {
   final remoteConfig = FirebaseRemoteConfig.instance;
   // setting FirebaseRemoteConfig update params
   await remoteConfig.setConfigSettings(RemoteConfigSettings(
-    fetchTimeout: const Duration(minutes: 1),
-    minimumFetchInterval: const Duration(hours: 1),
+    fetchTimeout: const Duration(seconds: 10),
+    minimumFetchInterval: const Duration(seconds: 10),
   ));
+  await remoteConfig.fetchAndActivate();
   return remoteConfig;
 }
