698a768ce46b39c54e1629d8eb94988a39ffa2d1
Altereyo <r8.lebz@gmail.com>
2023-05-24 17:53:18 +0300

    code: news json and model

--- lib/data/models/news.dart
+++ lib/data/models/news.dart
@@ -1,11 +1,15 @@
 class News {
+  final int? id;
   final String? title;
   final String? description;
+  final String? imageUrl;
 
 //<editor-fold desc="Data Methods">
   const News({
+    this.id,
     this.title,
     this.description,
+    this.imageUrl,
   });
 
   @override
@@ -13,38 +17,49 @@ class News {
       identical(this, other) ||
       (other is News &&
           runtimeType == other.runtimeType &&
+          id == other.id &&
           title == other.title &&
-          description == other.description);
+          description == other.description &&
+          imageUrl == other.imageUrl);
 
   @override
-  int get hashCode => title.hashCode ^ description.hashCode;
+  int get hashCode =>
+      id.hashCode ^ title.hashCode ^ description.hashCode ^ imageUrl.hashCode;
 
   @override
   String toString() {
-    return 'News{ title: $title, description: $description,}';
+    return 'News{ id: $id, title: $title, description: $description, imageUrl: $imageUrl,}';
   }
 
   News copyWith({
+    int? id,
     String? title,
     String? description,
+    String? imageUrl,
   }) {
     return News(
+      id: id ?? this.id,
       title: title ?? this.title,
       description: description ?? this.description,
+      imageUrl: imageUrl ?? this.imageUrl,
     );
   }
 
   Map<String, dynamic> toMap() {
     return {
+      'id': id,
       'title': title,
       'description': description,
+      'imageUrl': imageUrl,
     };
   }
 
   factory News.fromMap(Map<String, dynamic> map) {
     return News(
+      id: map['id'] as int,
       title: map['title'] as String,
       description: map['description'] as String,
+      imageUrl: map['imageUrl'] as String,
     );
   }
 
