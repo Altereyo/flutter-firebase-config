6160032fb839b59449e0c391b16f291e5193b7e6
Altereyo <r8.lebz@gmail.com>
2023-06-28 17:19:39 +0300

    changed images & colors, added background

--- lib/modules/plug/game_over.dart
+++ lib/modules/plug/game_over.dart
@@ -10,70 +10,76 @@ class GameOver extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height,
-      color: AppColors.backgroundColor,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            'Game Over',
-            style: TextStyle(
-              color: AppColors.gameOverTextColor,
-              fontSize: 50.0,
-              fontWeight: FontWeight.bold,
-              fontStyle: FontStyle.italic,
-              shadows: [
-                Shadow(
-                    // bottomLeft
-                    offset: Offset(-1.5, -1.5),
-                    color: Colors.black),
-                Shadow(
-                    // bottomRight
-                    offset: Offset(1.5, -1.5),
-                    color: Colors.black),
-                Shadow(
-                    // topRight
-                    offset: Offset(1.5, 1.5),
-                    color: Colors.black),
-                Shadow(
-                    // topLeft
-                    offset: Offset(-1.5, 1.5),
-                    color: Colors.black),
-              ],
-            ),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Positioned.fill(
+            child: Image.asset('assets/images/background.jpg', fit: BoxFit.cover),
           ),
-          const SizedBox(height: 50.0),
-          Text('Your Score is: $score',
-              style: const TextStyle(color: Colors.white, fontSize: 20.0)),
-          const SizedBox(height: 50.0),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              primary: AppColors.accentColor,
-            ),
-            onPressed: () {
-              Navigator.of(context).pushReplacement(
-                  MaterialPageRoute(builder: (context) => GamePage()));
-            },
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Icon(Icons.refresh, color: AppColors.textColor, size: 30.0),
-                const SizedBox(width: 10),
-                Text(
-                  'Try Again',
-                  style: TextStyle(
-                    color: AppColors.textColor,
-                    fontSize: 20,
-                  ),
-                )
-              ],
-            ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Game Over',
+                style: TextStyle(
+                  color: AppColors.gameOverTextColor,
+                  fontSize: 50.0,
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic,
+                  shadows: const [
+                    Shadow(
+                        // bottomLeft
+                        offset: Offset(-1.5, -1.5),
+                        color: Colors.black),
+                    Shadow(
+                        // bottomRight
+                        offset: Offset(1.5, -1.5),
+                        color: Colors.black),
+                    Shadow(
+                        // topRight
+                        offset: Offset(1.5, 1.5),
+                        color: Colors.black),
+                    Shadow(
+                        // topLeft
+                        offset: Offset(-1.5, 1.5),
+                        color: Colors.black),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 50.0),
+              Text('Your Score is: $score',
+                  style:
+                      const TextStyle(color: Colors.white, fontSize: 20.0)),
+              const SizedBox(height: 50.0),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: AppColors.accentColor,
+                ),
+                onPressed: () {
+                  Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (context) => GamePage()));
+                },
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(Icons.refresh,
+                        color: AppColors.textColor, size: 30.0),
+                    const SizedBox(width: 10),
+                    Text(
+                      'Try Again',
+                      style: TextStyle(
+                        color: AppColors.textColor,
+                        fontSize: 20,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
           ),
         ],
       ),
-    ));
+    );
   }
 }
--- lib/modules/plug/home_page.dart
+++ lib/modules/plug/home_page.dart
@@ -6,69 +6,79 @@ class PlugPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: AppColors.backgroundColor,
-        body: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'Welcome to\nBall Swap game!',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: AppColors.textColor,
-                  fontSize: 40.0,
-                  fontStyle: FontStyle.italic,
-                  fontWeight: FontWeight.bold,
-                  shadows: const [
-                    Shadow(
-                      // bottomLeft
-                        offset: Offset(-1.5, -1.5),
-                        color: Colors.black),
-                    Shadow(
-                      // bottomRight
-                        offset: Offset(1.5, -1.5),
-                        color: Colors.black),
-                    Shadow(
-                      // topRight
-                        offset: Offset(1.5, 1.5),
-                        color: Colors.black),
-                    Shadow(
-                      // topLeft
-                        offset: Offset(-1.5, 1.5),
-                        color: Colors.black),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 50.0),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: AppColors.accentColor,
-                ),
-                onPressed: () {
-                  Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context) => GamePage()));
-                },
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    const Icon(Icons.play_arrow,
-                        color: Colors.white, size: 30.0),
-                    const SizedBox(width: 10),
-                    Text(
-                      'Start game',
-                      style: TextStyle(
-                        color: AppColors.textColor,
-                        fontSize: 18,
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ],
+      body: Stack(
+        clipBehavior: Clip.none,
+        fit: StackFit.expand,
+        children: [
+          Positioned.fill(
+            child:
+                Image.asset('assets/images/background.jpg', fit: BoxFit.cover),
           ),
-        ));
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Welcome to\nFruit Swap game!',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: AppColors.textColor,
+                    fontSize: 40.0,
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.bold,
+                    shadows: const [
+                      Shadow(
+                          // bottomLeft
+                          offset: Offset(-1.5, -1.5),
+                          color: Colors.black),
+                      Shadow(
+                          // bottomRight
+                          offset: Offset(1.5, -1.5),
+                          color: Colors.black),
+                      Shadow(
+                          // topRight
+                          offset: Offset(1.5, 1.5),
+                          color: Colors.black),
+                      Shadow(
+                          // topLeft
+                          offset: Offset(-1.5, 1.5),
+                          color: Colors.black),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 50.0),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: AppColors.accentColor,
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => GamePage()));
+                  },
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const Icon(Icons.play_arrow,
+                          color: Colors.white, size: 30.0),
+                      const SizedBox(width: 10),
+                      Text(
+                        'Start game',
+                        style: TextStyle(
+                          color: AppColors.textColor,
+                          fontSize: 18,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
