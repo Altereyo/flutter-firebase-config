954a5ed5f953a5c337e499fe9d45f76e7b5777c1
Altereyo <r8.lebz@gmail.com>
2023-07-14 16:48:33 +0300

    initial guess-color logic

--- lib/modules/plug/game_page.dart
+++ lib/modules/plug/game_page.dart
@@ -1,8 +1,7 @@
-import 'dart:async';
 import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_firebase_config/core/theme/app_colors.dart';
-import 'package:flutter_firebase_config/modules/plug/game_over.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class GamePage extends StatefulWidget {
   const GamePage({super.key});
@@ -12,11 +11,17 @@ class GamePage extends StatefulWidget {
 }
 
 class _GamePageState extends State<GamePage> {
-  List<String> tileImages = [];
-  Timer? timer;
+  List<Color> diamondColors = [
+    Colors.red,
+    Colors.blue,
+    Colors.green,
+    Colors.yellow,
+  ];
+  String diamondImage = 'assets/images/diamond.svg';
+  Color targetColor = Colors.red;
+  List<Color?> diamondGrid = [];
   int score = 0;
-  int timeRemaining = 15;
-  double opacityLevel = 0.0;
+  bool gameOver = false;
 
   @override
   void initState() {
@@ -25,145 +30,85 @@ class _GamePageState extends State<GamePage> {
   }
 
   void startGame() {
-    generateTileImages();
-    startTimer();
-  }
-
-  void generateTileImages() {
-    final List<String> images = ['assets/images/image_1.png', 'assets/images/image_2.png'];
-    tileImages = List<String>.generate(25, (index) {
-      return images[Random().nextInt(2)];
-    });
-  }
-
-  void startTimer() {
-    timer = Timer.periodic(const Duration(seconds: 1), (timer) {
-      setState(() {
-        if (timeRemaining > 0) {
-          timeRemaining--;
-        } else {
-          timer.cancel();
-          Navigator.of(context).pushReplacement(MaterialPageRoute(
-              builder: (context) => GameOver(score: score)));
-        }
-      });
-    });
-  }
-
-  void swapImage(int index) {
     setState(() {
-      tileImages[index] = (tileImages[index] == 'assets/images/image_1.png')
-          ? 'assets/images/image_2.png'
-          : 'assets/images/image_1.png';
-      checkGameOver();
+      targetColor = diamondColors[Random().nextInt(diamondColors.length)];
+      diamondGrid = List<Color?>.generate(
+          16, (index) => diamondColors[Random().nextInt(diamondColors.length)]);
+      score = 0;
+      gameOver = false;
     });
   }
 
-  void checkGameOver() {
-    if (tileImages.every((image) => image == tileImages[0])) {
-      timeRemaining += 5;
-      score += 1;
-      opacityLevel = 1.0;
-      Future.delayed(const Duration(milliseconds: 500), () {
-        setState(() {
-          generateTileImages();
-          opacityLevel = 0.0;
-        });
+  void selectDiamond(int index) {
+    if (diamondGrid[index] == targetColor) {
+      setState(() {
+        diamondGrid[index] = null;
+        score++;
+      });
+      if (!diamondGrid.contains(targetColor)) {
+        startGame();
+      }
+    } else {
+      setState(() {
+        gameOver = true;
       });
     }
   }
 
   @override
-  void dispose() {
-    timer?.cancel();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        title: const Text('Fruit Swap Game'),
-        iconTheme: IconThemeData(
-          color: AppColors.textColor, //change your color here
-        ),
-        centerTitle: false,
-        backgroundColor: Colors.transparent,
-
+        title: const Text('Diamond Game'),
       ),
-      body: Stack(
-        fit: StackFit.expand,
+      body: ListView(
+        // mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Positioned.fill(
-            child: Image.asset('assets/images/background.jpg', fit: BoxFit.cover),
-          ),
-          Column(
+          Row(
             children: [
-              const SizedBox(height: 70),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 14),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Score: $score',
-                          style: TextStyle(fontSize: 24, color: AppColors.textColor),
-                        ),
-                        Text(
-                          'Time Remaining: $timeRemaining',
-                          style: TextStyle(fontSize: 24, color: AppColors.textColor),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 20),
-                    Center(
-                      child: AnimatedOpacity(
-                        opacity: opacityLevel,
-                        duration: const Duration(milliseconds: 500),
-                        child: Container(
-                          width: 110,
-                          height: 110,
-                          decoration: BoxDecoration(
-                            color: AppColors.accentColor,
-                            borderRadius: BorderRadius.circular(100),
-                          ),
-                          child: Center(
-                            child: Text(
-                              '+10\nseconds!',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                fontSize: 22,
-                                color: AppColors.textColor,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    )
-                  ],
-                ),
+              const Text(
+                'Find all diamonds:',
+                style: TextStyle(fontSize: 20),
               ),
-              const SizedBox(height: 20),
-              GridView.count(
-                padding: EdgeInsets.zero,
-                shrinkWrap: true,
-                crossAxisCount: 5,
-                children: List.generate(tileImages.length, (index) {
-                  return GestureDetector(
-                    onTap: () => swapImage(index),
-                    child: Container(
-                      margin: const EdgeInsets.all(4),
-                      child: Image.asset(tileImages[index]),
-                    ),
-                  );
-                }),
-              ),
+              const SizedBox(height: 10),
+              if (targetColor != null)
+                SvgPicture.asset(
+                  diamondImage,
+                  height: 60,
+                  width: 60,
+                  color: targetColor,
+                )
+              else
+                Container(),
             ],
+          ),
+          const SizedBox(height: 20),
+          GridView.builder(
+            shrinkWrap: true,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 4,
+            ),
+            itemCount: diamondGrid.length,
+            itemBuilder: (BuildContext context, int index) {
+              return GestureDetector(
+                onTap: () {
+                  if (!gameOver) {
+                    selectDiamond(index);
+                  }
+                },
+                child: Container(
+                  margin: const EdgeInsets.all(5),
+                  child: diamondGrid[index] != null
+                      ? SvgPicture.asset(
+                          diamondImage,
+                          height: 10,
+                          width: 10,
+                          color: diamondGrid[index]!,
+                        )
+                      : null,
+                ),
+              );
+            },
           ),
         ],
       ),
